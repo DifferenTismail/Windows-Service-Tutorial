f10320c9999273a0fd935633b93e05eebbc4b808
İsmail Baran <ismailbaran04@gmail.com>
2024-06-13 14:23:17 +0300

    We wrote the code lines that will run in the cmd screen here and showed some file paths.

--- WindowsServiceTutorial/Service1.cs
+++ WindowsServiceTutorial/Service1.cs
@@ -7,11 +7,14 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using System.IO;
 
 namespace WindowsServiceTutorial
 {
     public partial class Service1 : ServiceBase
     {
+        Timer timer = new Timer();
         public Service1()
         {
             InitializeComponent();
@@ -19,10 +22,43 @@ namespace WindowsServiceTutorial
 
         protected override void OnStart(string[] args)
         {
+            WriteToFile("Service is started at " + DateTime.Now);
+            timer.Elapsed += new ElapsedEventHandler(OnElapsedTime);
+            timer.Interval = 5000; //milisaniye
+            timer.Enabled = true;
         }
 
         protected override void OnStop()
         {
+            WriteToFile("Service is stoped at " + DateTime.Now);
+        }
+        private void OnElapsedTime(object source, ElapsedEventArgs e)
+        {
+            WriteToFile("Service was ran at " + DateTime.Now);
+        }
+        public void WriteToFile(string Message)
+        {
+            string path = AppDomain.CurrentDomain.BaseDirectory + "\\Logs";
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            string filePath = AppDomain.CurrentDomain.BaseDirectory + "\\Logs\\ServiceLog_" + DateTime.Now.ToShortDateString().Replace("/", "_") + ".txt";
+            if (!File.Exists(filePath))
+            {
+                //yazı için dosya oluştur
+                using (StreamWriter sw = File.CreateText(filePath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
+            else
+            {
+                using (StreamWriter sw = File.AppendText(filePath))
+                {
+                    sw.WriteLine(Message);
+                }
+            }
         }
     }
 }
